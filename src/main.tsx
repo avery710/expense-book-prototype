f3ad5833f205b862dbe65a4e93d3fa5ad66e1870
Avery Lin <angellinbalabababa@gmail.com>
2025-06-26 22:52:32 +0800

    feat(main): integrate Redux provider at app entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./stores/store";
 import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "@/components/ui/sonner";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-    <Toaster />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+      <Toaster position="bottom-center" />
+    </Provider>
+  </React.StrictMode>
 );
